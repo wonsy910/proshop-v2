9b0ba83b63095482283ca7c474ff5f503a3a7ddd
Daniel S. Won <wonsy910@gmail.com>
2023-08-10 22:39:27 -0400

    fetch products

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,24 @@
 import React from "react";
+import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {Link} from "react-router-dom";
 import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
+
   const {id: productId} = useParams();
-  const product = products.find(p => p._id === productId);
-  console.log(product._id);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
@@ -27,8 +37,9 @@ const ProductScreen = () => {
             <ListGroup.Item>
               <Rating value={product.rating} text={`${product.numReviews} reviews`} />
             </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
-          <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
         </Col>
         <Col md={3}>
           <Card>
